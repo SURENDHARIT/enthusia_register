c32ebb5336348be41636502da461f9d7d493de50
SURENDHARIT <95042574+SURENDHARIT@users.noreply.github.com>
2023-02-10 15:08:24 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var connectDB = async () => {
   await mongoose.connect(
 //     "mongodb+srv://sakthi:sakthi@enthusia.xzi3zhw.mongodb.net/test",
-    mongodb+srv://surendhar:surendhar@cluster0.fyt3kru.mongodb.net/prelims,
+    mongodb+srv://surendhar:surendhar@enthusia.fyt3kru.mongodb.net/prelims,
     {
       useNewUrlParser: true,
     }
