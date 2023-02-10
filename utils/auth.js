665da95a606320847053c89449107e1091fcc23f
SURENDHARIT <95042574+SURENDHARIT@users.noreply.github.com>
2023-02-10 12:09:00 +0530

    Delete utils directory

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = isAuthenticated = (req, res, next) => {
-  console.log(req.session.userid);
-  if (req.session.userid) {
-    console.log("Authenticated");
-    next();
-  } else {
-    res.redirect("/");
-  }
-};
